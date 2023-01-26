1b1246f050d08c0902b70928cf3ac56f2148f608
Sean McKinney <seanamckinney8@gmail.com>
2023-01-26 14:21:30 -0600

    Add a Virtual ApplicationUser Property to Item Add a Virtual ICollection<Item> Property to ApplicationUser

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WishList.Models
 {
     public class Item
     {
         public int Id { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
 
         [Required]
         [MaxLength(50)]

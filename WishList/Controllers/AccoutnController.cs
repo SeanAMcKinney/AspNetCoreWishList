825153c040c3befbc96250cc089b1e9e61f784d4
Sean McKinney <seanamckinney8@gmail.com>
2023-01-26 12:45:43 -0600

    finished creating the accountcontroler

---
+++ WishList/Controllers/AccoutnController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class AccoutnController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccoutnController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager= userManager;
+            _signInManager= signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

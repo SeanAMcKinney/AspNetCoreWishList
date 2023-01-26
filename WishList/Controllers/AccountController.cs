91f526220bbf262ab1e4b032848fb9b0308a8153
Sean McKinney <seanamckinney8@gmail.com>
2023-01-26 13:50:08 -0600

    Check PasswordSignInAsync Results In HttpPost Login Action Create a New Action Logout in the AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -61,10 +61,23 @@ namespace WishList.Controllers
         public IActionResult Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
+                return View(model);
+            var result = _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false).Result;
+            if (!result.Succeeded)
             {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt");
                 return View(model);
             }
+
             return RedirectToAction("Index", "Item");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
